1cf348f5936e44be0c1a38741dc3641e3d23300a
JM-AK <komlevalexandr87@gmail.com>
2020-06-13 21:07:41 +0300

    [Урок 7] Задача по сливанию файлов

--- HomeWork/lesson7/src/main/java/ru/gb/lesson7/MainClass.java
+++ HomeWork/lesson7/src/main/java/ru/gb/lesson7/MainClass.java
@@ -1,4 +1,37 @@
 package ru.gb.lesson7;
 
+/*
+Рекомендуемое ДЗ:
+1. Создать 2 текстовых файла, примерно по 50-100 символов в каждом (особого значения не имеет);
+2. Написать программу, «склеивающую» эти файлы, то есть вначале идет текст из первого файла, потом текст из второго в новый файл.
+3. * Написать программу, которая проверяет присутствует ли указанное пользователем слово в файле (работаем только с латиницей).
+4. ** Написать метод, проверяющий, есть ли указанное слово в файлах в папке
+5. *** Написать метод, добавляющий, указанное слово в файлы в папке
+ */
+
+
 public class MainClass {
+
+    public static void main(String[] args) {
+
+        String dirPath = "C:\\Users\\komle\\Рабочий стол\\test for java\\";
+        concatTwoFilesToNew (dirPath, "text_1.txt", "text_2.txt", "text_3.txt");
+
+    }
+
+    public static void concatTwoFilesToNew (String dirPath, String firstFile, String secondFile, String outFileName){
+        TxtFileClass t1 = new TxtFileClass(dirPath, firstFile);
+        TxtFileClass t2 = new TxtFileClass(dirPath, secondFile);
+        TxtFileClass t3 = new TxtFileClass(dirPath, outFileName);
+
+        if (t3.getTextFromFile().length() > 0) {
+            System.out.println("File \"" + outFileName + "\" exists, choose another name ");
+        } else {
+            t3.addTextToFile(t1.getTextFromFile());
+            t3.addTextToFile(t2.getTextFromFile());
+            System.out.println("Files were concatenated");
+        }
+
+    }
+
 }
---
+++ HomeWork/lesson7/src/main/java/ru/gb/lesson7/TxtFileClass.java
@@ -0,0 +1,89 @@
+package ru.gb.lesson7;
+
+/*
+* Класс для создания и редактирования txt файлов
+*
+* */
+
+import java.io.*;
+
+class TxtFileClass {
+
+    private final String dirPath;
+    private final String fileName;
+    private final String absolutePath;
+
+
+    public TxtFileClass(String dirPath, String fileName){
+        this.dirPath = dirPath;
+        this.fileName = fileName;
+        this.absolutePath = dirPath + fileName;
+        createTxtFile(dirPath, fileName);
+    }
+
+    public String getDirPath() {
+        return dirPath;
+    }
+    public String getFileName() {
+        return fileName;
+    }
+    public String getAbsolutePath() {
+        return absolutePath;
+    }
+
+    //метод создает путой txt файл
+    private static void createTxtFile (String dirPath, String fileName){
+        String absoluteFilePath = dirPath + fileName;
+        File file = new File(absoluteFilePath);
+        try {
+            if (file.createNewFile()) System.out.println("File was created");
+            else System.out.println("File \"" + fileName + "\" wasn't created. File exists");
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    //метод извлекает текст из файла по абсолютной ссылке
+    public String getTextFromFile (){
+        StringBuilder sOut = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(absolutePath));
+            String s;
+
+            while((s = reader.readLine()) != null){
+                sOut.append(s);
+                sOut.append("\n");
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sOut.toString();
+    }
+
+    //метод перезаписывает текст в файл по абсолютной ссылке
+    public void writeTextToFile (String text){
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(absolutePath));
+
+            writer.write(text);
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addTextToFile (String text){
+        StringBuilder strB = new StringBuilder();
+        strB.append(getTextFromFile());
+        strB.append(text);
+        writeTextToFile(strB.toString());
+    }
+
+    public boolean isContainWord (String word){
+
+        return false;
+    }
+
+}
